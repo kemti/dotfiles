4e865ceb6e99e9a80108255fbfbde8bdc24ccbd3
kemti <kempin.tim@googlemail.com>
2024-05-26 17:17:16 +0200

    update nvim config

--- .chezmoitemplates/nvim/lua/plugins/core.lua
+++ .chezmoitemplates/nvim/lua/plugins/core.lua
@@ -1,8 +1,16 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			window = {
+				position = "right",
+			},
+		},
+	},
 }
