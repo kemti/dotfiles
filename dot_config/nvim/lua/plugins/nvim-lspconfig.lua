ef41fde931dce41ab75a87a74be8c9417f12bf59
kemti <kempin.tim@googlemail.com>
2026-01-31 11:12:18 +0100

    Update .config/nvim/lua/plugins/nvim-lspconfig.lua

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,21 +1,32 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      html = {
-        filetypes = { "html", "templ", "htmldjango" },
-        init_options = {
-          configurationSection = { "html", "css", "javascript" },
-          embeddedLanguages = {
-            css = true,
-            javascript = true,
-          },
-          userLanguages = {
-            htmldjango = "html",
-          },
-          provideFormatter = true,
-        },
-      },
-    },
-  },
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			html = {
+				filetypes = { "html", "templ", "htmldjango" },
+				init_options = {
+					configurationSection = { "html", "css", "javascript" },
+					embeddedLanguages = {
+						css = true,
+						javascript = true,
+					},
+					userLanguages = {
+						htmldjango = "html",
+					},
+					provideFormatter = true,
+				},
+			},
+			omnisharp = {
+				settings = {
+					FormattingOptions = {
+						EnableEditorConfigSupport = true,
+					},
+					RoslynExtensionsOptions = {
+						-- Enables support for roslyn analyzers, code fixes and rulesets.
+						EnableAnalyzersSupport = true,
+					},
+				},
+			},
+		},
+	},
 }
