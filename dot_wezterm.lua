1f720afbfc045df2e4456706f47d05e4da293a95
kemti <kempin.tim@googlemail.com>
2026-05-30 12:34:53 +0200

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -39,36 +39,32 @@ config.window_padding = {
 -- KEY MAPS
 -------------------------------------------------------------------------------
 
-config.leader = { key = "Space", mods = "CTRL" }
+config.leader = { key = "g", mods = "CTRL" }
+
 config.keys = {
-
-	-- TAB ----------------------------------------------------------------------
-
-	-- spawn tab
 	{
-		key = "c",
+		key = "s",
 		mods = "LEADER",
-		action = act.SpawnTab("CurrentPaneDomain"),
+		action = act.QuickSelect,
 	},
-	-- close current tab
 	{
-		key = "&",
+		key = "y",
 		mods = "LEADER",
-		action = act.CloseCurrentTab({ confirm = true }),
+		action = act.ActivateCopyMode,
 	},
-	-- previous tab
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.ReloadConfiguration,
+	},
 	{
 		key = "p",
 		mods = "LEADER",
-		action = act.ActivateTabRelative(-1),
+		action = act.ActivateKeyTable({
+			name = "pane",
+			one_shot = false,
+		}),
 	},
-	-- next tab
-	{
-		key = "n",
-		mods = "LEADER",
-		action = act.ActivateTabRelative(1),
-	},
-	-- go into tab mode
 	{
 		key = "t",
 		mods = "LEADER",
@@ -77,206 +73,262 @@ config.keys = {
 			one_shot = false,
 		}),
 	},
+}
 
-	-- PANE ---------------------------------------------------------------------
+config.key_tables = {
+	pane = {
+		-- split horizontal (right)
+		{
+			key = "n",
+			action = act.Multiple({
+				act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+				act.PopKeyTable,
+			}),
+		},
+		{
+			key = "|",
+			action = act.Multiple({
+				act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+				act.PopKeyTable,
+			}),
+		},
 
-	-- split vertical
-	{
-		key = '"',
-		mods = "LEADER",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		-- split vertical (down)
+		{
+			key = "-",
+			action = act.Multiple({
+				act.SplitVertical({ domain = "CurrentPaneDomain" }),
+				act.PopKeyTable,
+			}),
+		},
+
+		-- close
+		{
+			key = "x",
+			action = act.Multiple({
+				act.CloseCurrentPane({ confirm = true }),
+				act.PopKeyTable,
+			}),
+		},
+
+		-- toggle fullscreen
+		{
+			key = "f",
+			action = act.Multiple({
+				act.TogglePaneZoomState,
+				act.PopKeyTable,
+			}),
+		},
+
+		-- quick selection
+		{
+			key = "q",
+			action = act.Multiple({
+				act.PaneSelect({ mode = "Activate" }),
+				act.PopKeyTable,
+			}),
+		},
+
+		-- swap
+		{
+			key = "s",
+			action = act.Multiple({
+				act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }),
+				act.PopKeyTable,
+			}),
+		},
+
+		-- move to new tab
+		{
+			key = "b",
+			action = act.Multiple({
+				act.PaneSelect({ mode = "MoveToNewTab" }),
+				act.PopKeyTable,
+			}),
+		},
+
+		-- move to new window
+		{
+			key = "b",
+			mods = "SHIFT",
+			action = act.Multiple({
+				act.PaneSelect({ mode = "MoveToNewWindow" }),
+				act.PopKeyTable,
+			}),
+		},
+
+		-- change focus
+		{
+			key = "h",
+			action = act.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "j",
+			action = act.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "k",
+			action = act.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "l",
+			action = act.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "LeftArrow",
+			action = act.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "DownArrow",
+			action = act.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "UpArrow",
+			action = act.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "RightArrow",
+			action = act.ActivatePaneDirection("Right"),
+		},
+
+		-- resize
+		{
+			key = "h",
+			mods = "SHIFT",
+			action = act.AdjustPaneSize({ "Left", 1 }),
+		},
+		{
+			key = "j",
+			mods = "SHIFT",
+			action = act.AdjustPaneSize({ "Down", 1 }),
+		},
+		{
+			key = "k",
+			mods = "SHIFT",
+			action = act.AdjustPaneSize({ "Up", 1 }),
+		},
+		{
+			key = "l",
+			mods = "SHIFT",
+			action = act.AdjustPaneSize({ "Right", 1 }),
+		},
+		{
+			key = "LeftArrow",
+			mods = "SHIFT",
+			action = act.AdjustPaneSize({ "Left", 1 }),
+		},
+		{
+			key = "DownArrow",
+			mods = "SHIFT",
+			action = act.AdjustPaneSize({ "Down", 1 }),
+		},
+		{
+			key = "UpArrow",
+			mods = "SHIFT",
+			action = act.AdjustPaneSize({ "Up", 1 }),
+		},
+		{
+			key = "RightArrow",
+			mods = "SHIFT",
+			action = act.AdjustPaneSize({ "Right", 1 }),
+		},
+
+		-- pop key table
+		{
+			key = "Escape",
+			action = act.PopKeyTable,
+		},
 	},
-	-- split horizontal
-	{
-		key = "%",
-		mods = "LEADER",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	-- close pane
-	{
-		key = "x",
-		mods = "LEADER",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	-- show pane selection
-	{
-		key = "q",
-		mods = "LEADER",
-		action = act.PaneSelect({ mode = "Activate" }),
-	},
-	-- swap selected pane with active pane
-	{
-		key = "s",
-		mods = "LEADER",
-		action = act.PaneSelect({ mode = "SwapWithActive" }),
-	},
-	-- move pane to new tab
-	-- {
-	-- 	key = "!",
-	-- 	mods = "LEADER",
-	-- 	action = act.PaneSelect({ mode = "MoveToNewTab" }),
-	-- },
-	-- toggle pane zoom
-	{
-		key = "z",
-		mods = "LEADER",
-		action = act.TogglePaneZoomState,
-	},
-	-- activate pane: left
-	{
-		key = "h",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	-- activate pane: right
-	{
-		key = "l",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	-- activate pane: up
-	{
-		key = "k",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	-- activate pane: down
-	{
-		key = "j",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	-- go into pane mode
-	{
-		key = "P",
-		mods = "LEADER",
-		action = act.ActivateKeyTable({
-			name = "pane",
-			one_shot = false,
-		}),
+
+	tab = {
+		-- new
+		{
+			key = "n",
+			action = act.Multiple({
+				act.SpawnTab("CurrentPaneDomain"),
+				act.PopKeyTable,
+			}),
+		},
+
+		-- close
+		{
+			key = "x",
+			action = act.Multiple({
+				act.CloseCurrentTab({ confirm = true }),
+				act.PopKeyTable,
+			}),
+		},
+
+		-- quick selection
+		{
+			key = "q",
+			action = act.Multiple({
+				act.ShowTabNavigator,
+				act.PopKeyTable,
+			}),
+		},
+
+		-- change focus
+		{
+			key = "h",
+			action = act.ActivateTabRelative(-1),
+		},
+		{
+			key = "l",
+			action = act.ActivateTabRelative(1),
+		},
+		{
+			key = "LeftArrow",
+			action = act.ActivateTabRelative(-1),
+		},
+		{
+			key = "RightArrow",
+			action = act.ActivateTabRelative(1),
+		},
+
+		-- move
+		{
+			key = "h",
+			mods = "SHIFT",
+			action = act.MoveTabRelative(-1),
+		},
+		{
+			key = "l",
+			mods = "SHIFT",
+			action = act.MoveTabRelative(1),
+		},
+		{
+			key = "LeftArrow",
+			mods = "SHIFT",
+			action = act.MoveTabRelative(-1),
+		},
+		{
+			key = "RightArrow",
+			mods = "SHIFT",
+			action = act.MoveTabRelative(1),
+		},
+
+		-- toggle last
+		{
+			key = "Tab",
+			action = act.ActivateLastTab,
+		},
+
+		-- pop key table
+		{
+			key = "Escape",
+			action = act.PopKeyTable,
+		},
 	},
 }
--- activate tab: 0..7
-for i = 1, 8 do
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "LEADER",
-		action = act.ActivateTab(i - 1),
-	})
-end
 
 -- Show which key table is active in the status area
-wezterm.on("update-right-status", function(window, pane)
+wezterm.on("update-right-status", function(window, _)
 	local name = window:active_key_table()
 	if name then
 		name = "TABLE: " .. name
 	end
 	window:set_right_status(name or "")
 end)
-
-config.key_tables = {
-	pane = {
-		-- split vertical
-		{ key = '"', action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-
-		-- split horizontal
-		{ key = "%", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-
-		-- close pane
-		{ key = "x", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
-
-		-- show pane selection
-		{ key = "q", action = act.PaneSelect({ mode = "Activate" }) },
-
-		-- swap selected pane with active pane
-		{ key = "s", action = act.PaneSelect({ mode = "SwapWithActive" }) },
-
-		-- move pane to new tab
-		-- {	key = "!",	action = act.PaneSelect({ mode = "MoveToNewTab" })	},
-
-		-- toggle pane zoom
-		{ key = "z", action = act.TogglePaneZoomState },
-
-		-- activate pane: left
-		{ key = "LeftArrow", action = act.ActivatePaneDirection("Left") },
-		{ key = "h", action = act.ActivatePaneDirection("Left") },
-
-		-- activate pane: right
-		{ key = "RightArrow", action = act.ActivatePaneDirection("Right") },
-		{ key = "l", action = act.ActivatePaneDirection("Right") },
-
-		-- activate pane: up
-		{ key = "UpArrow", action = act.ActivatePaneDirection("Up") },
-		{ key = "k", action = act.ActivatePaneDirection("Up") },
-
-		-- activate pane: down
-		{ key = "DownArrow", action = act.ActivatePaneDirection("Down") },
-		{ key = "j", action = act.ActivatePaneDirection("Down") },
-
-		-- resize pane: left
-		{ key = "LeftArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "h", mods = "CTRL", action = act.AdjustPaneSize({ "Left", 1 }) },
-
-		-- resize pane: right
-		{ key = "RightArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "l", mods = "CTRL", action = act.AdjustPaneSize({ "Right", 1 }) },
-
-		-- resize pane: up
-		{ key = "UpArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "k", mods = "CTRL", action = act.AdjustPaneSize({ "Up", 1 }) },
-
-		-- resize pane: down
-		{ key = "DownArrow", mods = "CTRL", action = act.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "j", mods = "CTRL", action = act.AdjustPaneSize({ "Down", 1 }) },
-
-		-- exit pane mode
-		{ key = "Escape", action = "PopKeyTable" },
-	},
-	tab = {
-		-- spawn tab
-		{ key = "c", action = act.SpawnTab("CurrentPaneDomain") },
-
-		-- close current tab
-		{ key = "&", action = act.CloseCurrentTab({ confirm = true }) },
-
-		-- previous tab
-		{ key = "p", action = act.ActivateTabRelative(-1) },
-
-		-- next tab
-		{ key = "n", action = act.ActivateTabRelative(1) },
-
-		-- move tab: left
-		{ key = "p", mods = "CTRL", action = act.MoveTabRelative(-1) },
-
-		-- move tab: right
-		{ key = "n", mods = "CTRL", action = act.MoveTabRelative(1) },
-
-		-- go into tab mode
-		{
-			key = "t",
-			action = act.ActivateKeyTable({
-				name = "tab",
-				one_shot = false,
-			}),
-		},
-
-		-- exit tab mode
-		{ key = "Escape", action = "PopKeyTable" },
-	},
-}
-
-for i = 1, 8 do
-	-- activate tab: 0..7 (tab mode)
-	table.insert(config.key_tables.tab, {
-		key = tostring(i),
-		action = act.ActivateTab(i - 1),
-	})
-	-- move tab to position
-	table.insert(config.key_tables.tab, {
-		key = tostring(i),
-		mods = "CTRL",
-		action = act.MoveTab(i - 1),
-	})
-end
 
 return config
